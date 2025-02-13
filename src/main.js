7c7edb74baf86d82477d8a47cb5c5a1e5ad2a44e
Julio Suarez <juliorbk.dev@gmail.com>
2025-02-13 11:11:14 -0400

    first push

---
+++ src/main.js
@@ -0,0 +1,42 @@
+let yesButton = document.getElementById("yes-button");
+let noButton = document.getElementById("no-button");
+let question = document.getElementById("text");
+let img = document.getElementById("img-1")
+let flag = false;
+let index = 0;
+
+yesButton.addEventListener("click", () => {
+  question.innerText = "Sabia que dirias que si,  Te amo"
+  img.src = "./src/image-2.gif"
+  yesButton.style.display = "none";
+  noButton.style.display = "none";
+  question.style.fontSize = "100px";
+})
+
+const growButton = () => {
+  let currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = (currentSize * 1.5) + "px";
+}
+
+noButton.addEventListener("click", () => {
+  let currentSize = parseFloat(window.getComputedStyle(noButton).fontSize);
+  noButton.style.fontSize = (currentSize / 1.1) + "px";
+  growButton();
+  showMessage(index);
+  changePic(index);
+  index++;
+})
+
+function showMessage(index) {
+  let messages = ["Estas segura?", "¿ya no me amas?", "yo te amo mucho:(", "terminaras diciendo que si porque me amas jijiji"];
+  if (index < messages.length) {
+    question.innerText = messages[index];
+  }
+}
+
+function changePic(index) {
+  let url = ["./src/image-3.png", "./src/image-4.gif", "./src/image-5.gif", "./src/image-6.gif"];
+  if (index < url.length) {
+    img.src = url[index];
+  }
+}
